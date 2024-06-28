19d61741c3c5b348ac1b1659cd41a23db9a0f18d
Kaushik Kakdey <kaushik.kakdey@gmail.com>
2024-06-29 00:36:40 +0530

    feat: added go lsp

--- init.lua
+++ init.lua
@@ -487,7 +487,47 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				-- gopls = {},
+				gopls = {
+					on_attach = function(client, bufnr)
+						local function buf_set_keymap(...)
+							vim.api.nvim_buf_set_keymap(bufnr, ...)
+						end
+						local function buf_set_option(...)
+							vim.api.nvim_buf_set_option(bufnr, ...)
+						end
+
+						buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+						-- Mappings.
+						local opts = { noremap = true, silent = true }
+
+						buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+						buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+						buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+						buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+						buf_set_keymap("n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+						buf_set_keymap("n", "<space>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+						buf_set_keymap("n", "<space>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+						buf_set_keymap(
+							"n",
+							"<space>wl",
+							"<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+							opts
+						)
+						buf_set_keymap("n", "<space>D", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+						buf_set_keymap("n", "<space>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+						buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+						buf_set_keymap("n", "<space>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
+						buf_set_keymap("n", "[d", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+						buf_set_keymap("n", "]d", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+						buf_set_keymap("n", "<space>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+						buf_set_keymap("n", "<space>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+					end,
+
+					flags = {
+						debounce_text_changes = 150,
+					},
+				},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
